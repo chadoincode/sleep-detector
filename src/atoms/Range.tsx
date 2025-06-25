51c4fe60501e6d241e2437d7e95e4c9ba5d266ab
shaa/cha(?!) <ayeshacha177@gmail.com>
2025-06-25 15:30:59 +0700

    chore: add range component

---
+++ src/atoms/Range.tsx
@@ -0,0 +1,21 @@
+const Range = () => {
+  return(
+    <div className="w-full max-w-xs">
+      <input type="range" min={0} max="100" value="25" className="range bg-primary" step="25" />
+      <div className="flex justify-between px-2.5 mt-2 text-xs">
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+        <span>|</span>
+      </div>
+      <div className="flex justify-between px-2.5 mt-2 text-xs">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+      </div>
+    </div>
+  )
+}
